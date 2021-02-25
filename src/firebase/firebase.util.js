c8dc334652fb42c6c914f74c7eb9525e9c158cc5
adriankonopko <adrian.konopko@gmail.com>
2021-02-25 12:43:56 +0100

    add firebase with google login initial

---
+++ src/firebase/firebase.util.js
@@ -0,0 +1,26 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyBvoUT9x0Cvx7iaT1B-R0wYUYkBKJtnHRQ",
+    authDomain: "crown-db-11cfb.firebaseapp.com",
+    projectId: "crown-db-11cfb",
+    storageBucket: "crown-db-11cfb.appspot.com",
+    messagingSenderId: "22167156091",
+    appId: "1:22167156091:web:f84fb43ad7a94d892579de",
+    measurementId: "G-81NVWNS6X9"
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+
+provider.setCustomParameters({ prompt: 'select_account' });
+
+export const signInWithGoogle = auth.signInWithPopup(provider);
+
+export default firebase;
